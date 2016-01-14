75945f0fe60eb431c9fc1cb88afcafc5853cd492
Dominic Cuneo <cuneo94@gmail.com>
2016-01-13 21:02:13 -0700

    stated audience and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,7 +9,10 @@
 				<p> This website's Purpose is to help me find some kind of gainful employment. Which will help me refine and
 				increase my skills making me more knowledgeable and marketable.  </p>
 		<h2>Audience</h2>
-		<p>My</p>
+		<p>My audience is anyone looking to hire full stack developer or anyone looking for a developer of any kind</p>
+		<h3>Goal</h3>
+		<p>My goal with this website is to help me find employment does not matter what kind as long as it furthers my skills
+		as a developer.</p>
 	</body
 
 </html
