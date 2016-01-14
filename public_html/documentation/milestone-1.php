44ddea06cdd7f6c57f9cdb8af885fffe2451ea21
Dominic Cuneo <cuneo94@gmail.com>
2016-01-13 20:46:50 -0700

    stated Purpose

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,11 +2,14 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title> My web page </title>
+		<title> Dominic Cuneo </title>
 	</head>
 	<body>
-		<h1>Hello world</h1>
-				<p> yep </p>
+		<h1>Purpose</h1>
+				<p> My Purpose is to become a full stack web developer. Using the skills I learn in this program I Plan to
+				obtain a job to use the skills. I have learned and to refine my skills and increase my abilites.</p>
+		<h2>Audience</h2>
+		<p></p>
 	</body
 
 </html
