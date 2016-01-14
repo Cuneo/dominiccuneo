d2b3340a0a6a9b1bebb27bdc2fdc5dcc03ccf9d5
Dominic Cuneo <cuneo94@gmail.com>
2016-01-13 20:53:51 -0700

    changed Purpose

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,10 +6,10 @@
 	</head>
 	<body>
 		<h1>Purpose</h1>
-				<p> My Purpose is to become a full stack web developer. Using the skills I learn in this program I Plan to
-				obtain a job to use the skills. I have learned and to refine my skills and increase my abilites.</p>
+				<p> This website's Purpose is to help me find some kind of gainful employment. Which will help me refine and
+				increase my skills making me more knowledgeable and marketable.  </p>
 		<h2>Audience</h2>
-		<p></p>
+		<p>My</p>
 	</body
 
 </html
