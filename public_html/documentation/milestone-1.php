788f691be8051abb52e574da01e82967203d3674
Dominic Cuneo <cuneo94@gmail.com>
2016-01-13 20:19:02 -0700

    added directories and milestone.php file

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title> My web page </title>
+	</head>
+	<body>
+		<h1>Hello world</h1>
+				<p> yep </p>
+	</body
+
+</html
