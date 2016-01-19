83adf90806cf0c705be0cff4ba405d6fd207fa80
Dominic Cuneo <cuneo94@gmail.com>
2016-01-19 10:01:07 -0700

    added to footer and modified footer and header box size

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,9 +11,9 @@
 				<p> This website's Purpose is to help me find some kind of gainful employment. Which will help me refine and
 				increase my skills making me more knowledgeable and marketable.  </p>
 		<h2>Audience</h2>
-		<p>My audience is anyone looking to hire full stack developer or anyone looking for a developer of any kind</p>
+				<p>My audience is anyone looking to hire full stack developer or anyone looking for a developer of any kind</p>
 		<h3>Goal</h3>
-		<p>My goal with this website is to help me find employment does not matter what kind as long as it furthers my skills as a developer.</p>
+				<p>My goal with this website is to help me find employment.The type of job is inconsequential so long as it refine and increase my skills  as a developer.  </p>
 		<h4>Persona </h4>
 		<ul>
 			<li> Narvaez Jr.</li>
@@ -27,7 +27,17 @@
 			<li>Boisterous</li>
 		</ul>
 		<p>Mr.Narvaez would view my site to see my skills while searching for someone to help him maintain his website and other services.  </p>
-		<footer>table</footer>
+		<footer>Contact information  and Interests <table>
+				<tr>
+					<td><a href="http://roosterteeth.com/">RoosterTeeth</a></td>
+					<td><a href="http://achievementhunter.com/">Achievement Hunter </a></td>
+					<td><a href="http://www.twitch.tv/brownman">BigBrown</a></td>
+				</tr>
+				<tr>
+					<td>Phone:(256)-555-2713 </td>
+					<td> Email: cueno####@gamil.com</td>
+				</tr>
+			</table></footer>
 	</body>
 
 </html
