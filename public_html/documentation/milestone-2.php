0825a998ffd7854d439b801d6e0b1994e354df2b
Dominic Cuneo <cuneo94@gmail.com>
2016-01-23 22:20:17 -0700

    content stratagey

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Title</title>
+	</head>
+	<body>
+<h1>Content Strategy</h1>
+		<p>The site will be short preferably one page it will have a few pictures about places with significance to me and my skills. Those pictures will be links to those places and the footer will have my contanct info and a list of intersts. </p>
+	</body>
+</html>
+
