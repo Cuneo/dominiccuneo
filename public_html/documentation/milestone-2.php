873d5590a1889762cd7f2f0d5ef1337efdfc1acf
Dominic Cuneo <cuneo94@gmail.com>
2016-01-28 17:40:46 -0700

    doo hickys

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,6 +7,8 @@
 	<body>
 <h1>Content Strategy</h1>
 		<p>The site will be short preferably one page it will have a few pictures about places with significance to me and my skills. Those pictures will be links to those places and the footer will have my contanct info and a list of intersts. </p>
+		<img src="images/wireframes.svg" alt="wireframe">
+		<img src="images/sitemap-interactionflow.svg" alt="interaction flow-sitemap">
 	</body>
 </html>
 
